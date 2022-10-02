7ffd43607157132c019279f9ff2f850813c37c37
hp <794731517@qq.com>
2022-10-02 08:32:36 +0800

    first vulkan with glow

--- VulkanTesting/main.cpp
+++ VulkanTesting/main.cpp
@@ -1,14 +1,57 @@
-//
-//  main.cpp
-//  VulkanTesting
-//
-//  Created by hp on 2022/10/2.
-//
-
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanUp();
+        
+    }
+private:
+    GLFWwindow* window;
+    
+    void initWindow(){
+        glfwInit();
+        
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        
+        window = glfwCreateWindow(WIDTH, HEIGHT, "vulkan", nullptr, nullptr);
+        
+    }
+    void initVulkan() {
+        
+    }
+    
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+        
+    }
+    
+    void cleanUp() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+    
+};
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main () {
+    HelloTriangleApplication app;
+    
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
 }
